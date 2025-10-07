0a4640c8b735929c6da2eb431c275023ba1e70e3
Lena Le <ll224ve@student.lnu.se>
2025-10-08 00:06:55 +0200

    feat(quiz): add QuizApp web component with initial shadow DOM and event listener structure

---
+++ src/js/components/quiz-app/quiz-app.js
@@ -0,0 +1,51 @@
+/**
+ * The quiz-application web component module.
+ *
+ * @author Lena Le <ll224ve@student.lnu.se>
+ * @version 1.0.0
+ */
+
+class QuizApp extends HTMLElement {
+  /**
+   * Creates an instance of QuizApp.
+   */
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  /**
+   * Called when the component is added to the DOM.
+   * Initializes the quiz application by setting up the HTML structure and attaching event listeners for handling user interactions.
+   */
+  connectedCallback() {
+    const template = document.createElement('template')
+    template.innerHTML = `
+      <style>
+        .hidden {
+          display: none;
+        }
+      </style>
+      
+      <nickname-form></nickname-form> <!--add hidden when quiz starts-->
+      <quiz-question class="hidden"></quiz-question> <!--remove hidden when quiz starts-->
+      <countdown-timer class="hidden"></countdown-timer> <!--remove hidden when quiz starts-->
+      <high-score class="hidden"></high-score> <!--remove hidden when quiz starts-->
+    `
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.attachEventListeners()
+  }
+
+  /**
+   * Attaches event listeners for user interactions
+   */
+  attachEventListeners() {}
+
+  // async getQuestion() 
+  // async handleAnswer()
+  // stopTimer
+  // endQuiz()
+  // restartQuiz()
+}
+
+customElements.define('quiz-app', QuizApp)
