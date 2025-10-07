034b6511630dac748a9e4081cdf30171b7ef387c
Lena Le <ll224ve@student.lnu.se>
2025-10-07 12:14:56 +0200

    chore: fix file structure and initial set up for index.html and index.js

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Lena Le <ll224ve@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import './components/quiz-app/quiz-app.js'
+
+console.log('index.js loaded!')
