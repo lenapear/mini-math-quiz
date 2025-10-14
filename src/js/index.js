2c398b37b2a081fd0e3f5403210fe7a1c8387352
Lena Le <ll224ve@student.lnu.se>
2025-10-14 14:40:24 +0200

    import countdown-timer web component in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,6 @@ import './components/quiz-app/quiz-app.js'
 import './components/nickname-form/nickname-form.js'
 import './components/difficulty-form/difficulty-form.js'
 import './components/quiz-question/quiz-question.js'
+import './components/countdown-timer/countdown-timer.js'
 
 console.log('index.js loaded!')
